b889777fc44e48d459d5df1aedccdc45c956fb07
monpkz <monip0907@gmail.com>
2021-07-03 20:13:01 -0400

    add package e imports en los repos

--- src/main/java/ar/com/ada/api/questionados/repos/PreguntaRepository.java
+++ src/main/java/ar/com/ada/api/questionados/repos/PreguntaRepository.java
@@ -1,5 +1,11 @@
+package ar.com.ada.api.questionados.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ar.com.ada.api.questionados.entities.*;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PreguntaRepository extends JpaRepository<Pregunta, Integer> {
+   
     
 }
--- src/main/java/ar/com/ada/api/questionados/repos/RespuestaRepository.java
+++ src/main/java/ar/com/ada/api/questionados/repos/RespuestaRepository.java
@@ -1,3 +1,10 @@
+package ar.com.ada.api.questionados.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ar.com.ada.api.questionados.entities.Respuesta;
+
 @RepuestaRepository
 public interface RespuestaRepository extends JpaRepository<Respuesta, Integer> {
 
