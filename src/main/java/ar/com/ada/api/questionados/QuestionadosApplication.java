d06a310388422a0614f40808e7f3fdd609c46a23
monpkz <monip0907@gmail.com>
2021-08-03 21:44:25 -0400

    //

--- src/main/java/ar/com/ada/api/questionados/QuestionadosApplication.java
+++ src/main/java/ar/com/ada/api/questionados/QuestionadosApplication.java
@@ -3,6 +3,7 @@ package ar.com.ada.api.questionados;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Api Rest inspirada en el juego Preguntados
 @SpringBootApplication
 public class QuestionadosApplication {
 
